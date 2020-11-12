b433a8c6a5c094782f637bd7850c4b95e62a9ac8
davidsmiller1 <davidsmiller1@gmail.com>
2020-11-11 22:35:19 -0600

    Commit 3
    Dashboard updates

---
+++ js/index.js
@@ -0,0 +1,59 @@
+var i = 0;
+function move1() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar1");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+}
+
+var i = 0;
+function move2() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar2");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+}
+
+var i = 0;
+function move3() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar3");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+}
